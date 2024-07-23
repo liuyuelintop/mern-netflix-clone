71ebe16e47d0f83af69e7b2c92e21bded7022ac6
liuyuelintop <liuyuelintop@gmail.com>
2024-07-23 17:33:09 +1000

    9. Search Routes

---
+++ backend/controllers/search.controller.js
@@ -0,0 +1,131 @@
+import { User } from "../models/user.model.js";
+import { fetchFromTMDB } from "../services/tmdb.service.js";
+
+export async function searchPerson(req, res) {
+  const { query } = req.params;
+  try {
+    const response = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`
+    );
+
+    if (response.results.length === 0) {
+      return res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: response.results[0].id,
+          image: response.results[0].profile_path,
+          title: response.results[0].name,
+          searchType: "person",
+          createdAt: new Date(),
+        },
+      },
+    });
+
+    res.status(200).json({ success: true, content: response.results });
+  } catch (error) {
+    console.log("Error in searchPerson controller: ", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function searchMovie(req, res) {
+  const { query } = req.params;
+
+  try {
+    const response = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`
+    );
+
+    if (response.results.length === 0) {
+      return res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: response.results[0].id,
+          image: response.results[0].poster_path,
+          title: response.results[0].title,
+          searchType: "movie",
+          createdAt: new Date(),
+        },
+      },
+    });
+    res.status(200).json({ success: true, content: response.results });
+  } catch (error) {
+    console.error("Error in searchMovie controller: ", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function searchTv(req, res) {
+  const { query } = req.params;
+
+  try {
+    const response = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`
+    );
+
+    if (response.results.length === 0) {
+      return res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: response.results[0].id,
+          image: response.results[0].poster_path,
+          title: response.results[0].name,
+          searchType: "tv",
+          createdAt: new Date(),
+        },
+      },
+    });
+    res.status(200).json({ success: true, content: response.results });
+  } catch (error) {
+    console.error("Error in searchTv controller: ", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function getSearchHistory(req, res) {
+  try {
+    res.status(200).json({ success: true, content: req.user.searchHistory });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function removeItemFromSearchHistory(req, res) {
+  let { id } = req.params; // type of id: string
+
+  id = parseInt(id);
+  //id may not in history, check it before removing
+  const itemExists = req.user.searchHistory.some((item) => item.id === id);
+  if (!itemExists) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Item not found in search history" });
+  }
+
+  try {
+    await User.findByIdAndUpdate(req.user._id, {
+      $pull: {
+        searchHistory: { id: id },
+      },
+    });
+
+    res
+      .status(200)
+      .json({ success: true, message: "Item removed from search history" });
+  } catch (error) {
+    console.error(
+      "Error in removeItemFromSearchHistory controller: ",
+      error.message
+    );
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 import cookieParser from "cookie-parser";
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
   connectDB();
