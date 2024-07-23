79b82a24b5515d210dbda1f9007d200d57acedc6
liuyuelintop <liuyuelintop@gmail.com>
2024-07-23 17:36:04 +1000

    Correct ENV_VARS.NODE_ENV

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -6,6 +6,6 @@ export const ENV_VARS = {
   MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT || 5000,
   JWT_SECRET: process.env.JWT_SECRET,
-  NodeJS_ENV: process.env.NODE,
+  NODE_ENV: process.env.NODE_ENV,
   TMDB_API_KEY: process.env.TMDB_API_KEY,
 };
