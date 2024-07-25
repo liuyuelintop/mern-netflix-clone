9776c78c5c8b097ce254549aadf48750234146e5
liuyuelintop <liuyuelintop@gmail.com>
2024-07-25 17:55:27 +1000

    15. Building the Watch page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast"
 import { useAuthStore } from "./store/authUser"
 import { useEffect } from "react"
 import { Loader } from "lucide-react"
+import WatchPage from "./pages/WatchPage"
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -30,6 +31,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path='/login' element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
         <Route path='/signup' element={!user ? <SignupPage /> : <Navigate to={"/"} />} />
+        <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
       </Routes>
       <Toaster />
       <Footer />
