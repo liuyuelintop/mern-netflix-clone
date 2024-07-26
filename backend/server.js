cbe902343b4bcfbb62844687e32c8af5794ee0b8
liuyuelintop <liuyuelintop@gmail.com>
2024-07-26 11:39:24 +1000

    17. Deployment SetUp

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
+
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
-import cookieParser from "cookie-parser";
 import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
 const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -18,6 +23,15 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
   connectDB();
