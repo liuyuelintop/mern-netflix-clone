ed24e61fbe4ec7ae9bacebfac90b7048d668cf71
liuyuelintop <liuyuelintop@gmail.com>
2024-07-26 10:39:34 +1000

    16. Building SearchPage, SearchHistoryPage and NotFoundPage

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # mern-netflix-clone
 
 [![1721878819768](image/README/1721878819768.png)](https://github.com/liuyuelintop/mern-netflix-clone)
+
+## TODO
+
+- [ ] ActorPage
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,10 @@ import { useAuthStore } from "./store/authUser"
 import { useEffect } from "react"
 import { Loader } from "lucide-react"
 import WatchPage from "./pages/WatchPage"
+import SearchPage from "./pages/SearchPage"
+import SearchHistoryPage from "./pages/SearchHistoryPage"
+import NotFoundPage from "./pages/404";
+
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -32,6 +36,9 @@ function App() {
         <Route path='/login' element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
         <Route path='/signup' element={!user ? <SignupPage /> : <Navigate to={"/"} />} />
         <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+        <Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+        <Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+        <Route path='/*' element={<NotFoundPage />} />
       </Routes>
       <Toaster />
       <Footer />
