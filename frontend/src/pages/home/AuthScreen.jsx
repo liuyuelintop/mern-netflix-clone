32a56457421069c76349a18d05c578940bcffd90
liuyuelintop <liuyuelintop@gmail.com>
2024-08-01 16:01:40 +1000

    chore: Rebrand and remove trademark violations, rename project to NetWatch

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -14,7 +14,7 @@ const AuthScreen = () => {
 
             {/* Navbar */}
             <header className='max-w-6xl mx-auto flex items-center justify-between p-4 pb-10'>
-                <img src='/netflix-logo.png' alt='Netflix Logo' className='w-32 md:w-52' />
+                <img src='/netwatch-logo.png' alt='NetWatch Logo' className='w-32 md:w-60' />
                 <Link to={"/login"} className='text-white bg-red-600 py-1 px-2 rounded'>
                     Sign In
                 </Link>
